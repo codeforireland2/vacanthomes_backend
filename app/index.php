74033a5007bd874404b1cdc4f8caed324d57fb44
Luke Kearney <luke.t.kearney@gmail.com>
2018-06-27 19:53:44 +0100

    upgrade to slim 3. cleaning up :)

--- app/index.php
+++ app/index.php
@@ -1,56 +1,57 @@
 <?php
 
-require_once "../Slim/Slim.php";
-Slim\Slim::registerAutoloader ();
+require '../vendor/autoload.php';
 
-$app = new \Slim\Slim (); // slim run-time object
+$app = new \Slim\App; // slim run-time object
 
 require_once "conf/config.inc.php";
 
 //function to authenticate user
-function authenticate() {
+// function authenticate() {
 	
-	require_once 'models/UserModel.php';
-	require_once 'controllers/UserController.php';
-	require_once 'views/generalView.php';
+// 	require_once 'models/UserModel.php';
+// 	require_once 'controllers/UserController.php';
+// 	require_once 'views/generalView.php';
 	
-	$app = \Slim\Slim::getInstance ();
-	$headers = $app->request->headers;
+// 	$app = \Slim\Slim::getInstance ();
+// 	$headers = $app->request->headers;
 	
-	if(($headers ["user"] != null) && ($headers ["password"] != null))
-	{
+// 	if(($headers ["user"] != null) && ($headers ["password"] != null))
+// 	{
 		
-		$action = ACTION_AUTHENTICATE;
-		$parameters ["body"] = null;
-		$parameters ["header"] ["user"] = $headers ["user"]; 
-		$parameters ["header"] ["password"] = $headers ["password"]; 
+// 		$action = ACTION_AUTHENTICATE;
+// 		$parameters ["body"] = null;
+// 		$parameters ["header"] ["user"] = $headers ["user"]; 
+// 		$parameters ["header"] ["password"] = $headers ["password"]; 
 		
-		//create model and controller
-		$model = new UserModel(); // common model
-		$controller = new UserController($model, $action, $app, $parameters );
+// 		//create model and controller
+// 		$model = new UserModel(); // common model
+// 		$controller = new UserController($model, $action, $app, $parameters );
 		
-		//check authentication successful
-		if($model->successfulAuthentication == true) {
-			return true;
-		}
-		else {
-			//read header
-			$encoding = $app->request->getContentType();
+// 		//check authentication successful
+// 		if($model->successfulAuthentication == true) {
+// 			return true;
+// 		}
+// 		else {
+// 			//read header
+// 			$encoding = $app->request->getContentType();
 			
-			//write and send response
-			$view = new GeneralView( $controller, $model, $app, $app->headers ); // common view
-			$view->output ($encoding); 
-			$app->stop();
-		}	
-	}
-	else {
-		$app->stop();
-	}
-}
+// 			//write and send response
+// 			$view = new GeneralView( $controller, $model, $app, $app->headers ); // common view
+// 			$view->output ($encoding); 
+// 			$app->stop();
+// 		}	
+// 	}
+// 	else {
+// 		$app->stop();
+// 	}
+// }
+$app->get("/", function($request, $response, $args) {
+	return $response->withStatus(200)->write('Hello World!');
+});
 
-$app->map ( "/properties/search(/:county)", 'authenticate', function ($county = null) use($app) {
-	
-	$httpMethod = $app->request->getMethod ();
+$app->get( "/properties/search/{county}", function ($request, $response, $args) {
+	$county = $args['county'];
 	$action = null;
 	$parameters ["body"] = null;
 	$parameters ["SearchingString"] = $county;
@@ -58,100 +59,89 @@ $app->map ( "/properties/search(/:county)", 'authenticate', function ($county = 
 	if ($county != null) {
 		$action = ACTION_SEARCH_PROPERTIES;
 	}
-	return new loadRunMVCComponents ( "PropertyModel", "PropertyController", "GeneralView", $action, $app, $parameters );
-	
-})->via ( "GET" );
+	return $response->withStatus(200)->write($county);
+});
 
-$app->map ( "/properties(/:id)", 'authenticate', function ($propertyID = null) use($app) {
-	
-	$httpMethod = $app->request->getMethod ();
+$app->any( "/properties/{id}", function ($request, $response, $args) {
+	$httpMethod = $request->getMethod();
+	$propertyID = $args['id'];
 	$action = null;
-	$parameters ["id"] = $propertyID; // prepare parameters to be passed to the controller (example: ID)
+	$parameters["id"] = $propertyID; // prepare parameters to be passed to the controller (example: ID)
 	
 	//read header
-	$encoding = $app->request->getContentType();
+	$encoding = $request->getContentType();
 	
 	//decode body
-	$body = $app->request->getBody (); // get the body of the HTTP request (from client)
+	$body = $request->getBody (); // get the body of the HTTP request (from client)
 	$parameters ["body"] = decodeBody($body, $encoding, "property");
 	
-	
 	if (($propertyID == null) or is_numeric ( $propertyID )) {
-		switch ($httpMethod) {
-			case "GET" :
-				if ($propertyID != null)
-					$action = ACTION_GET_PROPERTY;
-				else
-					$action = ACTION_GET_PROPERTIES;
-				break;
-			case "POST" :
-				$action = ACTION_CREATE_PROPERTY;
-				break;
-			// case "PUT" :
-				// $action = ACTION_UPDATE_PROPERTY;
-				// break;
-			case "DELETE" :
-				$action = ACTION_DELETE_PROPERTY;
-				break;
-			default :
+		if ($request->isGet()){
+			if ($propertyID != null)
+				$action = ACTION_GET_PROPERTY;
+			else
+				$action = ACTION_GET_PROPERTIES;
+		} elseif ($request->isPost()) {
+			$action = ACTION_CREATE_PROPERTY;
+		} elseif ($request->isDelete()) {
+			$action = ACTION_DELETE_PROPERTY;
 		}
 	}
-	
-	return new loadRunMVCComponents ( "PropertyModel", "PropertyController", "GeneralView", $action, $app, $encoding, $parameters);
-} )->via ( "GET", "POST", "PUT", "DELETE" );
+	return new loadRunMVCComponents ( "PropertyModel", "PropertyController", "GeneralView", $action, $this, $parameters );
+});
 
-//method for search of users
-$app->map ( "/users/search(/:prefix)", function ($prefix = null) use($app) {
+// //method for search of users
+// $app->map ( "/users/search(/:prefix)", function ($prefix = null) use($app) {
 	
-	$httpMethod = $app->request->getMethod ();
-	$action = null;
-	$parameters ["body"] = null;
-	$parameters ["SearchingString"] = $prefix;
+// 	$httpMethod = $app->request->getMethod ();
+// 	$action = null;
+// 	$parameters ["body"] = null;
+// 	$parameters ["SearchingString"] = $prefix;
 	
-	if ($prefix != null) {
-		$action = ACTION_SEARCH_USERS;
-	}
-	return new loadRunMVCComponents ( "UserModel", "UserController", "GeneralView", $action, $app, $parameters );
+// 	if ($prefix != null) {
+// 		$action = ACTION_SEARCH_USERS;
+// 	}
+// 	return new loadRunMVCComponents ( "UserModel", "UserController", "GeneralView", $action, $app, $parameters );
 	
-})->via ( "GET" );
+// })->via ( "GET" );
 
-$app->map ( "/users(/:id)", function ($userID = null) use($app) {
+// $app->map ( "/users(/:id)", function ($userID = null) use($app) {
 	
-	$httpMethod = $app->request->getMethod ();
-	$action = null;
-	$parameters ["id"] = $userID; // prepare parameters to be passed to the controller (example: ID)
+// 	$httpMethod = $app->request->getMethod ();
+// 	$action = null;
+// 	$parameters ["id"] = $userID; // prepare parameters to be passed to the controller (example: ID)
 	
-	//read header
-	$encoding = $app->request->getContentType();
+// 	//read header
+// 	$encoding = $app->request->getContentType();
 	
-	//decode body
-	$body = $app->request->getBody (); // get the body of the HTTP request (from client)
-	$parameters ["body"] = decodeBody($body, $encoding, "user");
+// 	//decode body
+// 	$body = $app->request->getBody (); // get the body of the HTTP request (from client)
+// 	$parameters ["body"] = decodeBody($body, $encoding, "user");
 	
 	
-	if (($userID == null) or is_numeric ( $userID )) {
-		switch ($httpMethod) {
-			case "GET" :
-				if ($userID != null)
-					$action = ACTION_GET_USER;
-				else
-					$action = ACTION_GET_USERS;
-				break;
-			case "POST" :
-				$action = ACTION_CREATE_USER;
-				break;
-			case "PUT" :
-				$action = ACTION_UPDATE_USER;
-				break;
-			case "DELETE" :
-				$action = ACTION_DELETE_USER;
-				break;
-			default :
-		}
-	}
+// 	if (($userID == null) or is_numeric ( $userID )) {
+// 		switch ($httpMethod) {
+// 			case "GET" :
+// 				if ($userID != null)
+// 					$action = ACTION_GET_USER;
+// 				else
+// 					$action = ACTION_GET_USERS;
+// 				break;
+// 			case "POST" :
+// 				$action = ACTION_CREATE_USER;
+// 				break;
+// 			case "PUT" :
+// 				$action = ACTION_UPDATE_USER;
+// 				break;
+// 			case "DELETE" :
+// 				$action = ACTION_DELETE_USER;
+// 				break;
+// 			default :
+// 		}
+// 	}
 	
-	return new loadRunMVCComponents ( "UserModel", "UserController", "GeneralView", $action, $app, $encoding, $parameters);
-} )->via ( "GET", "POST", "PUT", "DELETE" );
+// 	return new loadRunMVCComponents ( "UserModel", "UserController", "GeneralView", $action, $app, $encoding, $parameters);
+// } )->via ( "GET", "POST", "PUT", "DELETE" );
 
 $app->run ();
 class loadRunMVCComponents {
